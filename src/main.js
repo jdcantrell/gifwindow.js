8a7a7d5023b58222585e4709b487de949f180246
JD Cantrell <jd@goodrobot.net>
2016-02-07 21:24:18 -0800

    Add loading indicators and lazy load of images

--- src/main.js
+++ src/main.js
@@ -1,35 +1,70 @@
-import Rx from 'rx';
+import Rx from 'rx-lite';
 
-function intent() {
+var getLoadedImages = function() {
+  var imgs = Array.prototype.slice.call(document.querySelectorAll('img'));
+  return imgs.filter(function (el) {
+      if (el.naturalHeight !== 0) {
+        return true;
+      }
+      else {
+        el.parentNode.parentNode.classList.add('loading');
+        return false;
+      }
+    });
+}
+
+var intent = function() {
   return {
     scroll: Rx.Observable.fromEvent(window, 'scroll')
       .map(ev => window.scrollY)
       .throttle(32)
-      .startWith(window.scrollY)
+      .startWith(window.scrollY),
+    load: Rx.Observable.fromEvent(document.querySelectorAll('img'), 'load')
+      .map(ev => ev.target)
   };
-}
+};
 
-function model(actions) {
+var positionModel = function(actions) {
   var top = 0
-  var bottom = window.innerHeight;
+  var bottom = window.innerHeight + 200;
   return actions.scroll.map(
     scrollPosition => ({top: top, bottom: bottom, position: scrollPosition})
   );
-}
+};
 
-function view(state) {
+var tileView = function(state) {
   var els = Array.prototype.slice.call(document.querySelectorAll('li'));
   return state.map(({top, bottom, position}) => {
-    els.forEach(el => {
+    els = els.filter(el => {
       var y = el.offsetTop - position;
-      if (y + el.offsetHeight > top && y < bottom) {
+      if (y < bottom) {
         el.classList.remove('offscreen');
+        var img = el.querySelector('img');
+        var src = img.dataset.src
+        if (src != img.src) {
+          img.src = src;
+        }
+        return false;
       }
       else {
         el.classList.add('offscreen');
+        return true;
       }
     });
   });
-}
+};
 
-view(model(intent())).subscribe();
+var loadingModel = function(actions) {
+  return actions.load.map(
+    el => el.parentNode.parentNode
+  ).startWith(...getLoadedImages())
+};
+
+var tileLoadingView = function (state) {
+  return state.map(el => {
+    el.classList.remove('loading');
+  });
+};
+
+tileView(positionModel(intent())).subscribe();
+tileLoadingView(loadingModel(intent())).subscribe();
